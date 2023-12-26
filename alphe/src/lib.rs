381c37037631fe4213a2e247f124b7d7ea760bda
bujosa <davidbujosa@gmail.com>
2023-12-26 18:18:15 -0400

    feat: implement  add_updated_at_field and validate_fields

--- alphe/src/lib.rs
+++ alphe/src/lib.rs
@@ -30,3 +30,112 @@ pub fn validate_name(_attr: TokenStream, item: TokenStream) -> TokenStream {
     // Return the modified function
     TokenStream::from(quote! { #function })
 }
+
+#[proc_macro_attribute]
+pub fn validate_lastname(_attr: TokenStream, item: TokenStream) -> TokenStream {
+    let mut function = parse_macro_input!(item as ItemFn);
+
+    // Parse the validation logic as a block of statements
+    let validation: Block = syn::parse_quote!({
+        let mut person_clone = person.clone();
+        let person = person_clone.into_inner();
+        if person.lastname.len() < 5 {
+            return rocket::http::Status::BadRequest;
+        }
+        let person = rocket::serde::json::Json(person);
+    });
+
+    // Prepend the validation logic to the original function body
+    let original_body = function.block;
+    function.block = syn::parse_quote!({
+        #validation
+        #original_body
+    });
+
+    // Return the modified function
+    TokenStream::from(quote! { #function })
+}
+
+#[proc_macro_attribute]
+pub fn validate_email(_attr: TokenStream, item: TokenStream) -> TokenStream {
+    let mut function = parse_macro_input!(item as ItemFn);
+
+    // Parse the validation logic as a block of statements
+    let validation: Block = syn::parse_quote!({
+        let mut person_clone = person.clone();
+        let person = person_clone.into_inner();
+        if !person.email.contains("@") {
+            return rocket::http::Status::BadRequest;
+        }
+        let person = rocket::serde::json::Json(person);
+    });
+
+    // Prepend the validation logic to the original function body
+    let original_body = function.block;
+    function.block = syn::parse_quote!({
+        #validation
+        #original_body
+    });
+
+    // Return the modified function
+    TokenStream::from(quote! { #function })
+}
+
+#[proc_macro_attribute]
+pub fn validate_fields(_attr: TokenStream, item: TokenStream) -> TokenStream {
+    let mut function = parse_macro_input!(item as ItemFn);
+
+    // Parse the validation logic as a block of statements
+    let validation: Block = syn::parse_quote!({
+        let mut person_clone = person.clone();
+        let person = person_clone.into_inner();
+        let mut errors = Vec::new();
+        if person.name.len() < 5 {
+            errors.push(String::from("Name must be at least 5 characters long"));
+        }
+        if person.lastname.len() < 5 {
+            errors.push(String::from("Lastname must be at least 5 characters long"));
+        }
+        if !person.email.contains("@") {
+            errors.push(String::from("Email must contain @"));
+        }
+        if !errors.is_empty() {
+            return Err(rocket::response::status::Custom(
+                rocket::http::Status::BadRequest,
+                rocket::serde::json::Json(errors),
+            ));
+        }
+        let person = rocket::serde::json::Json(person);
+    });
+
+    // Prepend the validation logic to the original function body
+    let original_body = function.block;
+    function.block = syn::parse_quote!({
+        #validation
+        #original_body
+    });
+
+    // Return the modified function
+    TokenStream::from(quote! { #function })
+}
+
+#[proc_macro_attribute]
+pub fn add_updated_at_field(_attr: TokenStream, item: TokenStream) -> TokenStream {
+    let mut function = parse_macro_input!(item as ItemFn);
+
+    // Parse the validation logic as a block of statements
+    let validation: Block = syn::parse_quote!({
+        person.updated_at = Some(String::from("2021-09-01"));
+    });
+
+
+    // Prepend the validation logic to the original function body
+    let original_body = function.block;
+    function.block = syn::parse_quote!({
+        #validation
+        #original_body
+    });
+
+    // Return the modified function
+    TokenStream::from(quote! { #function })
+}
